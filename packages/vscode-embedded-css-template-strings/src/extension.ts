4125aad39e2f47966b074e2e7fd240186d4c1216
Dan Hedgecock <8461733+DHedgecock@users.noreply.github.com>
2022-11-23 11:30:57 -0600

    Add VSCode extension for syntax highlighting

---
+++ packages/vscode-embedded-css-template-strings/src/extension.ts
@@ -0,0 +1,7 @@
+export function activate() {
+  console.log('Extension "vscode-embedded-css-template-strings" is now active')
+}
+
+export function deactivate() {
+  console.log('Extension "vscode-embedded-css-template-strings" will be de-activated')
+}
