37088ad93878f0fd638dc7fcbdd5d09be824f1fc
Dan Hedgecock <8461733+DHedgecock@users.noreply.github.com>
2022-12-06 05:48:06 -0800

    Scaffold out test application

---
+++ packages/cssup-webpack-loader/test/src/App.js
@@ -0,0 +1,10 @@
+import styles from './app.module.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <h1 className={styles.header}>Huzzah!</h1>
+      <h2 className={styles.subtitle}>Embedded CSS!</h2>
+    </div>
+  )
+}
---
+++ packages/cssup-webpack-loader/test/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const { NODE_ENV } = process.env
+
+module.exports = {
+  mode: NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+
+  // --------------------------------------------------------
+  // RULES+PLUGINS
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  // --------------------------------------------------------
+  // DEV SERVER
+
+  devServer: {
+    static: './dist',
+    historyApiFallback: true,
+  },
+}
